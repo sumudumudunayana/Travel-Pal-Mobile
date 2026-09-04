9747411e486d40650a31b7ae5cb5d27cccf2f8a6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-04 07:14:26 +0530

    auth service updated

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,5 @@
 import api from "./api";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const registerUser = async (userData: any) => {
   const response = await api.post("/auth/register", userData);
@@ -10,6 +11,20 @@ export const loginUser = async (email: string, password: string) => {
     email,
     password,
   });
+
+  return response.data;
+};
+
+export const changePassword = async (
+  currentPassword: string,
+  newPassword: string,
+) => {
+  const token = await AsyncStorage.getItem('token');
+  const response = await api.put(
+    '/auth/change-password',
+    {currentPassword, oldPassword: currentPassword, newPassword},
+    {headers: {Authorization: `Bearer ${token}`}},
+  );
 
   return response.data;
 };
